028c0fbcd62abbfd9f0b6d3cc972014dbf6e7003
Sebastian Stamm <sebastian.stamm@camunda.com>
2018-12-12 10:58:09 +0100

    add listing of available plugins

--- index.js
+++ index.js
@@ -1,28 +1,79 @@
+const template = `
+<div class="dashboard">
+  <h1>Plugin Store</h1>
+  <h2>Available Plugins</h2>
+  <ul>
+    <li ng-repeat="plugin in uninstalledPlugins" style="    list-style: none;
+    border: 1px solid black;
+    display: inline-block;
+    background-color: white;
+    padding: 15px;
+    height: 250px;
+    margin: 10px;
+    position: relative;
+    box-shadow: 5px 5px 5px lightgrey;">
+      <img src="https://thecatapi.com/api/images/get?size=small&rnd={{plugin.id}}" style="height: 100%;" />
+      <div style="float: right; margin-left: 15px; width: 300px;">
+        <h3>{{plugin.title}}</h3>
+        <p>{{plugin.description}}</p>
+        </div>
+      <button style="position: absolute; bottom: 15px; right: 15px;
+      display:inline-block;
+      padding:0.7em 1.4em;
+      margin:0 0.3em 0.3em 0;
+      border-radius:0.15em;
+      box-sizing: border-box;
+      text-decoration:none;
+      font-family:'Roboto',sans-serif;
+      text-transform:uppercase;
+      font-weight:400;
+      color:#FFFFFF;
+      background-color:#3369ff;
+      box-shadow:inset 0 -0.6em 0 -0.35em rgba(0,0,0,0.17);
+      text-align:center;
+      border: none;
+      width: 100px;
+      ">Install</button>
+    </li>
+  </ul>
+</div>
+`;
+
 define(["angular"], function(angular) {
   var ngModule = angular.module("cockpit.pluginStore", []);
 
   ngModule.config([
     "ViewsProvider",
     "$routeProvider",
-    "PluginsProvider",
-    function(ViewsProvider, routeProvider, PluginsProvider) {
+    function(ViewsProvider, routeProvider) {
       routeProvider.when("/plugins", {
-        template: "<p>Hello world</p>",
+        template,
         controller: [
           "$scope",
-          "Plugins",
-          function($scope, Plugins) {
+          function($scope) {
             $scope.$root.showBreadcrumbs = false;
-            console.log("Plugins", Plugins.getAllProviders("view"));
 
-            const groupedPlugins = Plugins.getAllProviders("view");
+            console.log("fetching all plugins");
+            $scope.availablePlugins = [
+              {
+                id: "a",
+                title: "Sample Plugin",
+                description:
+                  "This is a normal sample plugin. It conains a quite long description, so that we can test the line breaking"
+              },
+              {
+                id: "b",
+                title: "Another Sample Plugin",
+                description: "This is another sample plugin"
+              }
+            ];
 
-            const pluginCount = Object.values(groupedPlugins).reduce(
-              (acc, curr) => acc + curr.length,
-              0
+            console.log("fetching installed plugins");
+            $scope.installedPlugins = [];
+
+            $scope.uninstalledPlugins = $scope.availablePlugins.filter(
+              ({ id }) => !$scope.installedPlugins.includes(id)
             );
-
-            console.log("count", pluginCount);
           }
         ],
         authentication: "required"
@@ -37,8 +88,6 @@ define(["angular"], function(angular) {
           return path.indexOf("#/plugins") > -1;
         }
       });
-
-      console.log("plp", PluginsProvider);
     }
   ]);
 
