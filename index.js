b32f012302fbf6e2163b04727a555a7f81995bbd
Sebastian Stamm <sebastian.stamm@camunda.com>
2018-12-12 11:34:24 +0100

    add install button

--- index.js
+++ index.js
@@ -1,4 +1,83 @@
 const template = `
+<style>
+.installButton {
+  position: absolute; bottom: 15px; right: 15px;
+  display:inline-block;
+  padding:0.7em 1.4em;
+  margin:0 0.3em 0.3em 0;
+  border-radius:0.15em;
+  box-sizing: border-box;
+  text-decoration:none;
+  font-family:'Roboto',sans-serif;
+  text-transform:uppercase;
+  font-weight:400;
+  background-color:#3369ff;
+  color:#FFFFFF;
+  box-shadow:inset 0 -0.6em 0 -0.35em rgba(0,0,0,0.17);
+  text-align:center;
+  border: none;
+  width: 100px;
+  height: 40px;
+}
+.loader,
+.loader:before,
+.loader:after {
+  border-radius: 50%;
+  width: 2.5em;
+  height: 2.5em;
+  -webkit-animation-fill-mode: both;
+  animation-fill-mode: both;
+  -webkit-animation: load7 1.8s infinite ease-in-out;
+  animation: load7 1.8s infinite ease-in-out;
+}
+.loader {
+  color: #ffffff;
+  font-size: 5px;
+  margin: -18px auto;
+  position: relative;
+  text-indent: -9999em;
+  -webkit-transform: translateZ(0);
+  -ms-transform: translateZ(0);
+  transform: translateZ(0);
+  -webkit-animation-delay: -0.16s;
+  animation-delay: -0.16s;
+}
+.loader:before,
+.loader:after {
+  content: '';
+  position: absolute;
+  top: 0;
+}
+.loader:before {
+  left: -3.5em;
+  -webkit-animation-delay: -0.32s;
+  animation-delay: -0.32s;
+}
+.loader:after {
+  left: 3.5em;
+}
+@-webkit-keyframes load7 {
+  0%,
+  80%,
+  100% {
+    box-shadow: 0 2.5em 0 -1.3em;
+  }
+  40% {
+    box-shadow: 0 2.5em 0 0;
+  }
+}
+@keyframes load7 {
+  0%,
+  80%,
+  100% {
+    box-shadow: 0 2.5em 0 -1.3em;
+  }
+  40% {
+    box-shadow: 0 2.5em 0 0;
+  }
+}
+
+</style>
 <div class="dashboard">
   <h1>Plugin Store</h1>
   <h2>Available Plugins</h2>
@@ -17,23 +96,8 @@ const template = `
         <h3>{{plugin.title}}</h3>
         <p>{{plugin.description}}</p>
         </div>
-      <button style="position: absolute; bottom: 15px; right: 15px;
-      display:inline-block;
-      padding:0.7em 1.4em;
-      margin:0 0.3em 0.3em 0;
-      border-radius:0.15em;
-      box-sizing: border-box;
-      text-decoration:none;
-      font-family:'Roboto',sans-serif;
-      text-transform:uppercase;
-      font-weight:400;
-      color:#FFFFFF;
-      background-color:#3369ff;
-      box-shadow:inset 0 -0.6em 0 -0.35em rgba(0,0,0,0.17);
-      text-align:center;
-      border: none;
-      width: 100px;
-      ">Install</button>
+      <button class="installButton" ng-click="install(plugin)" ng-if="!isInstalling(plugin)">Install</button>
+      <button style="background-color:#ffc733; cursor: default;" class="installButton" ng-if="isInstalling(plugin)"><div class="loader">Loading</div></button>
     </li>
   </ul>
 </div>
@@ -74,6 +138,16 @@ define(["angular"], function(angular) {
             $scope.uninstalledPlugins = $scope.availablePlugins.filter(
               ({ id }) => !$scope.installedPlugins.includes(id)
             );
+
+            $scope.installingPlugins = [];
+
+            $scope.install = plugin => {
+              console.log("should install", plugin);
+              $scope.installingPlugins.push(plugin.id);
+            };
+
+            $scope.isInstalling = ({ id }) =>
+              $scope.installingPlugins.includes(id);
           }
         ],
         authentication: "required"
